2697fbcf8a1cc33a6c4580f9837d8fdbdbbbec26
tomjerbo <60573821+tomjerbo@users.noreply.github.com>
2020-12-19 13:27:00 +0100

    lul

---
+++ LargeMultiplayerGame/Assets/IceParticle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceParticle : MonoBehaviour
+{
+    // private float size = 0.3f;
+    // public Vector3 endPos;
+    // [SerializeField] private ParticleSystem particle;
+    // private Vector3 startPos;
+    //
+    // private float timer = 0;
+    // private void Update()
+    // {
+    //     transform.position = Vector3.Lerp(startPos, endPos, timer);
+    //     timer += Time.deltaTime;
+    //     particle.shape.radius = timer;
+    //     if (timer > 1) Destroy(this.gameObject);
+    //
+    // }
+}
--- LargeMultiplayerGame/Assets/SpellCasting.cs
+++ LargeMultiplayerGame/Assets/SpellCasting.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SpellCasting : MonoBehaviour
 {
     [SerializeField] private Transform castPoint;
-
+    [SerializeField] private GameObject ice;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) CastSpell();
@@ -14,10 +14,10 @@ public class SpellCasting : MonoBehaviour
 
     private void CastSpell()
     {
-        if (Physics.Raycast(castPoint.position, castPoint.forward, out RaycastHit hit,  100f))
+        if (Physics.Raycast(castPoint.position, castPoint.forward, out RaycastHit hit,  20f))
         {
+            Instantiate(ice, hit.point, Quaternion.identity);
             Debug.DrawLine(castPoint.position, hit.point, Color.green, 2f);
-
             RaycastHit[] sphereHit = Physics.SphereCastAll(hit.point, 5f, Vector3.up);
             foreach (var targets in sphereHit)
             {
